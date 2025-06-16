2fd855231df8347b466b293dab158937a00ee95b
tq320 <vzzn5y@gmail.com>
2025-06-15 21:50:34 -0700

    Improved responsiveness by changing all units in style.css to relative units. Finished structure of Resume section (Experience, Education, Skills, About Me), added script.js to maniuplate button display on click

---
+++ script.js
@@ -0,0 +1,14 @@
+const resumeBtns = document.querySelectorAll('.resume-btn')
+resumeBtns.forEach((btn, idx) => {
+	btn.addEventListener('click', () => {
+		const resumeDetails = document.querySelectorAll('.resume-detail');
+		resumeBtns.forEach(btn => {
+			btn.classList.remove('active')
+		})
+		btn.classList.add('active')
+		resumeDetails.forEach(btn => {
+			btn.classList.remove('active')
+		})
+		resumeDetails[idx].classList.add('active')
+	})
+})
